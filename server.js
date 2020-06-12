fc233f54342884db2589fe0aab29c20af0ca8973
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-06-12 12:21:16 -0400

    Was able to set up router and basic model functions to get list of current projects

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require("express");
 const server = express();
 server.use(express.json());
 
+const projectsRouter = require("./projects/projects-router.js")
+
+server.use("/api/projects", projectsRouter);
+
 server.get("/", (req, res) => {
     res.status(200).json("Connected!")
 })
